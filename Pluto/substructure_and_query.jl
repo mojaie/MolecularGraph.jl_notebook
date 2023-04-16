e56affa9061a12bb6e7d3a3cca6959c76ce435a4
Seiji Matsuoka <ms0ae8ti1sj2ui@gmail.com>
2023-04-16 17:48:15 +0900

    substr and query 2

--- Pluto/substructure_and_query.jl
+++ Pluto/substructure_and_query.jl
@@ -24,9 +24,9 @@ This tutorial includes following database search and substructure mining tasks
 - Substructure match
 - InChI and InChIKey
 - SMARTS query
+- Structural alerts (e.g. PAINS)
 - Functional group analysis
-- Structural alert (e.g. PAINS)
-- Query containment (experimental)
+- Query containment
 "
 
 # ╔═╡ 22c15ca6-784a-428f-b851-890f7146ff52
@@ -35,6 +35,14 @@ data_dir = let
 	data_dir = "_data"
 	isdir(data_dir) || mkdir(data_dir)
 	data_dir
+end
+
+# ╔═╡ 606ef609-5411-467f-8536-0d93ef2c409c
+function fetch_mol!(cid, name, datadir)
+	url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/$(cid)/SDF"
+	dest = joinpath(data_dir, "$(name).mol")
+	isfile(dest) || download(url, dest);
+	return dest
 end
 
 # ╔═╡ c371014c-2cc6-4542-9061-29bf6fade656
@@ -47,28 +55,27 @@ function fetch_sdf_by_sid!(sid, name, datadir)
 	return dest
 end
 
-# ╔═╡ 98d11d83-01c7-4e5c-8a44-6f3d06224985
+# ╔═╡ 43e8d598-3889-4954-a6ab-be936fca159a
 md"
 ### Substructure match
 
 - `has_exact_match(mol1, mol2)` returns whether the structures of `mol1` and `mol2` match exactly.
 - `has_substruct_match(mol1, mol2)` returns whether `mol2` is a substructure of `mol1`
-
 "
 
-# ╔═╡ dbb371af-fd50-48dd-b653-87095b6c2acc
+# ╔═╡ b175ed91-1cfd-48f4-a3e7-892be19c5661
 thiostrepton = sdftomol(fetch_sdf_by_sid!("175266353", "Thiostrepton", data_dir))
 
-# ╔═╡ 13dea6cc-6626-4a75-aeca-20b08908e8d9
+# ╔═╡ d081bb98-0b70-446f-b544-870ac305fdf8
 has_exact_match(thiostrepton, thiostrepton)
 
-# ╔═╡ a3b9fc71-3a64-44e3-8ba3-42ed860e3d8c
+# ╔═╡ 82d0ca4c-96b3-4792-8cc9-d9e3947fa4b4
 thiazole = smilestomol("s1cncc1")
 
-# ╔═╡ e24b5427-5f69-488c-ab76-7cd7c510f4b9
+# ╔═╡ 0c72e2ea-b6cb-46b1-bbcc-48e0bfa0a618
 has_exact_match(thiostrepton, thiazole)
 
-# ╔═╡ d3ad2ec6-1f02-4664-a9ed-ea1e79de7e40
+# ╔═╡ fe178836-f0d8-409b-baa0-61b34c257cbc
 has_substruct_match(thiostrepton, thiazole)
 
 # ╔═╡ b632e57f-6ee8-424c-a6c0-d75d0a285769
@@ -76,7 +83,7 @@ md"
 - `exact_matches(mol1, mol2)` and `substruct_matches(mol1, mol2)` returns an iterator that yields vertex mappings between the molecules.
 "
 
-# ╔═╡ 58584903-f737-4654-8e48-0d3b28e19fb9
+# ╔═╡ 69f7a580-bf68-41c6-ae96-6ecc587e058f
 let
 	matched_nodes = Set{Int}()
 	for mapping in substruct_matches(thiostrepton, thiazole)
@@ -88,7 +95,7 @@ let
 	html_fixed_size(svg, 300, 300)
 end
 
-# ╔═╡ e216758d-c84e-4cbb-9358-73e31fa53bdb
+# ╔═╡ 526dd590-2858-4eaa-a9a3-e77375e9ff7f
 md"
 ### InChI and InChIKey
 
@@ -96,20 +103,20 @@ md"
 - InChI and InChIKey provides structure-based identifier to chemical database and enables efficient exact structure match. See InChI Trust technical FAQ for usage and limitation of InChI and InChIKey [https://www.inchi-trust.org/technical-faq/](https://www.inchi-trust.org/technical-faq/)
 "
 
-# ╔═╡ d03046ef-5cdc-4bae-a9a1-ab3f282d305b
+# ╔═╡ 3187aaf2-0c17-40e0-909c-da5d5bf13a36
 inchi(thiostrepton)
 
-# ╔═╡ 50e3e3e2-aa10-432b-a8db-e9c3fe8298d1
+# ╔═╡ f563ce55-fcc6-4b18-9050-1617121d1c52
 inchikey(thiostrepton)
 
-# ╔═╡ 76f3acc5-3209-422c-9d67-8db8b24dd0c3
+# ╔═╡ f110144e-8685-4ac7-bde4-6f31f293981d
 md"
 ### SMARTS query
 
 SMARTS queries also can be used as an argument of substructure match methods.
 "
 
-# ╔═╡ 4e6c750d-2bf9-4f2b-ac28-8b2aeb1cb0d6
+# ╔═╡ e8391325-b7bc-4f4f-b4d6-5637adb75775
 let
 	matched_nodes = Set{Int}()
 	# nitrogens with one or two hydrogens connected
@@ -123,7 +130,7 @@ let
 	html_fixed_size(svg, 300, 300)
 end
 
-# ╔═╡ a649fb33-53e2-4e93-8267-be2a595a640f
+# ╔═╡ 3cd09b95-75d7-463d-a3d2-9af10f46eb78
 let
 	matched_nodes = Set{Int}()
 	# amides next to thiazoles
@@ -137,87 +144,148 @@ let
 	html_fixed_size(svg, 300, 300)
 end
 
-# ╔═╡ 760dca56-97a5-4074-aa1f-b7bdd7a38e2c
+# ╔═╡ cd47e23d-636a-453e-82eb-bf5bfa12c56d
 has_substruct_match(thiostrepton, smartstomol(raw"[$(Cc1ncsc1)](=O)N"))
-
-# ╔═╡ ab5213fb-da1f-453e-9db1-1972b829da9c
-md"
-### Functional group analysis
-
-
-"
 
 # ╔═╡ 19feb1b5-f810-4d84-92c3-4425ca157150
 md"
 ### Structural alerts
 
+- MolecularGraph.jl has default query sets for functional group detection and structural alerts (Substructures often avoided in medicinal chemistry. These are often called 'PAINS' but PAINS is one of those).
+- Note that the structural alerts are not a universal structural filter. Some of them are based solely on chemists' intuition, others are compounds that interfere with only some assay systems.
+- `query_containment_diagram(; sources=[])` generates query containment diagram (described later) from the default query sets which is used for the analysis.
+- The structural alerts dataset is from PubChem database. Which datasets to use can be specified by `sources` keyword arguments with the following keywords that indicate the origin: `BMS`, `Dundee`, `Glaxo`, `MLSMR` and `PAINS`. If `sources` is not specified or is an empty vector, all default datasets will be used.
 
 "
+
+# ╔═╡ 13090468-209e-43a0-97c0-6ffb9861f88f
+sa_diagram = query_containment_diagram(); nothing
+
+# ╔═╡ 28f5b1f7-a7f3-4278-b02d-fbb0603f52e7
+curcumin = smilestomol(raw"O=C(\C=C\c1ccc(O)c(OC)c1)CC(=O)\C=C\c2cc(OC)c(O)cc2");nothing
+
+# ╔═╡ 12899136-feef-43bd-867d-42f18a0486ad
+html_fixed_size(drawsvg(curcumin, atomindex=true), 500, 300)
+
+# ╔═╡ d6881fe7-9f91-4ea1-b7b3-00a4121e8445
+md"
+- `find_queries(mol, query_diagram; sources=[])` returns matched queries in the default datasets as a subgraph of the query containment diagram.
+"
+
+# ╔═╡ 43080b6d-d0a6-49d3-b8fc-c02daae5bab6
+curcumin_sa = find_queries(curcumin, sa_diagram; sources=["BMS", "Dundee", "Glaxo", "MLSMR", "PAINS"]); nothing
+
+# ╔═╡ e84d1e3b-4617-49bd-938a-593c77bd24a1
+let
+	results = []
+	for rcd in values(curcumin_sa[2])
+		push!(results, Dict(
+			"key" => rcd["key"],
+			"name" => rcd["info"][1]["name"],
+			"query" => rcd["info"][1]["query"],
+			"source" => rcd["info"][1]["source"],
+			"matched" => rcd["matched"]  # matched vertex indices
+		))
+	end
+	results
+end
+
+# ╔═╡ 1f5cf7ec-110e-40be-b5ef-a5b6ae33383c
+md"
+### Functional group analysis
+
+- Manually curated default functional group datasets are available for efficient caluclation and systematic annotaton of functional groups. These datasets can be specified in `sources` keyword of the `query_containment_diagram` method with the following keywords: `default`, `scaffold`, `biomolecule`
+"
+
+# ╔═╡ 50720c92-2082-45b5-af16-4c6b04b2ed67
+fg_diagram = query_containment_diagram(sources=["default", "scaffold", "biomolecule"]); nothing
+
+# ╔═╡ f9097463-464a-47ee-aaa3-5501513c044b
+cefditoren = let
+	molfile = fetch_mol!("6437877", "Cefditoren Pivoxil", data_dir)
+	mol = sdftomol(molfile)
+	remove_hydrogens!(mol)
+	mol
+end; nothing
+
+# ╔═╡ 61062c66-e4e0-4177-a2cd-21b84ac6c572
+html_fixed_size(drawsvg(cefditoren, atomindex=true), 500, 300)
+
+# ╔═╡ c151cb13-b17f-46c9-be32-328c2f794da0
+cefditoren_fg = find_queries(cefditoren, fg_diagram); nothing
+
+# ╔═╡ a928a761-91eb-4416-87a6-ca4c52d9636e
+let
+	results = Dict[]
+	for n in vertices(cefditoren_fg[1])
+	    indegree(cefditoren_fg[1], n) == 0 || continue
+		rcd = cefditoren_fg[2][n]
+	    push!(results, Dict(
+			"key" => rcd["key"],
+			"name" => rcd["info"][1]["name"],
+			"query" => rcd["info"][1]["query"],
+			"source" => rcd["info"][1]["source"],
+			"matched" => rcd["matched"]
+		))
+	end
+	results
+end
 
 # ╔═╡ 87b379c1-0740-4d63-b9d5-13db607d4e61
 md"
-### Query containment (experimental)
+### Query containment
 
+- Carbonyl group query (`[#6]=O`) always matches the molecule which matches carboxylate query (`O=CO`), similarly carboxylate query always matches the molecule which matches ester (`[$(C[#6])](=O)[$(O(C)[#6])]`). Thus, if clearly a particular query will return a subset of the search results for another query, the calculation can be omitted. MolecularGraph.jl implements a mechanism to resolve such query containment.
 "
 
-# ╔═╡ c2e69ed7-4f54-447b-b592-b65f7a2c567e
-mol = smartstomol("CCC(C)C1C(=O)NC(C(=O)NC(=C)C(=O)NC(C(=O)NC23CCC(=NC2C4=CSC(=N4)C(C(OC(=O)C5=NC6=C(C=CC(C6O)N1)C(=C5)C(C)O)C)NC(=O)C7=CSC(=N7)C(NC(=O)C8CSC(=N8)C(=CC)NC(=O)C(NC(=O)C9=CSC3=N9)C(C)O)C(C)(C(C)O)O)C1=NC(=CS1)C(=O)NC(=C)C(=O)NC(=C)C(=O)N)C)C");nothing
-
-# ╔═╡ 5c5673fc-14b6-4f25-85d2-bb4e7aaa434b
-sssr(mol)
-
-# ╔═╡ d67a7898-4574-422c-8af6-4e148951001b
-q1 = smartstomol("C")
-
-# ╔═╡ dc93d51e-cb6c-44e5-9f40-4cdb689146f1
-q2 = smartstomol(raw"[$([Ru]),$([Rh]),$([Se]),$([Pd]),$([Sc]),$([Bi]),$([Sb]),$([Ag]),$([Ti]),$([Al]),$([Cd]),$([V]),$([In]),$([Cr]),$([Sn]),$([Mn]),$([La]),$([Fe]),$([Er]),$([Tm]),$([Yb]),$([Lu]),$([Hf]),$([Ta]),$([W]),$([Re]),$([Co]),$([Os]),$([Ni]),$([Ir]),$([Cu]),$([Zn]),$([Ga]),$([Ge]),$([As]),$([Y]),$([Zr]),$([Nb]),$([Ce]),$([Pr]),$([Nd]),$([Sm]),$([Eu]),$([Gd]),$([Tb]),$([Dy]),$([Ho]),$([Pt]),$([Au]),$([Hg]),$([Tl]),$([Pb]),$([Ac]),$([Th]),$([Pa]),$([Mo]),$([U]),$([Tc]),$([Te]),$([Po]),$([At])]")
-
-# ╔═╡ 601f71ad-0d74-4708-9200-0834ce04aa8f
-MolecularGraph.optimize_query(q1.vprops[1])
-
-# ╔═╡ bcd4013f-e748-4154-8491-db66e8a8e0b9
-MolecularGraph.optimize_query(q2.vprops[1])
-
-# ╔═╡ 98b38ccb-6a09-4aec-abd2-284c1793fc42
-MolecularGraph.resolve_recursive(
-	q1.vprops[1].tree,
-	MolecularGraph.querypropmap(q1.vprops[1].tree)
+# ╔═╡ f3ec83c3-7034-4062-a41d-30c46ec5f309
+has_substruct_match(
+	smartstomol(raw"[$(C[#6])](=O)[$(O(C)[#6])]"),
+	smartstomol("O=CO")
 )
 
-# ╔═╡ 27839cba-d644-4180-bfbb-ba60c72038b7
-MolecularGraph.resolve_recursive(
-	q2.vprops[1].tree,
-	MolecularGraph.querypropmap(q2.vprops[1].tree)
-)
+# ╔═╡ 1d0bf723-3762-43f3-bda7-369ff1b8a326
+md"
+##### Limitation
+
+- In the current implementation, truthtable size and the query containment evaluation is ``O(2^n)`` where ``n`` is the number of query literals of an atom. To avoid hangup, default `maxsize` parameter of `generate_truthtable` method is set to 14.
+
+"
 
 # ╔═╡ Cell order:
 # ╟─3d9ec0c8-d7f8-11ed-1e08-0983ce040e4e
 # ╠═7f5ac271-a399-4ca2-a6e6-4ce08277ce41
 # ╠═22c15ca6-784a-428f-b851-890f7146ff52
+# ╠═606ef609-5411-467f-8536-0d93ef2c409c
 # ╠═c371014c-2cc6-4542-9061-29bf6fade656
-# ╠═98d11d83-01c7-4e5c-8a44-6f3d06224985
-# ╠═dbb371af-fd50-48dd-b653-87095b6c2acc
-# ╠═13dea6cc-6626-4a75-aeca-20b08908e8d9
-# ╠═a3b9fc71-3a64-44e3-8ba3-42ed860e3d8c
-# ╠═e24b5427-5f69-488c-ab76-7cd7c510f4b9
-# ╠═d3ad2ec6-1f02-4664-a9ed-ea1e79de7e40
+# ╟─43e8d598-3889-4954-a6ab-be936fca159a
+# ╠═b175ed91-1cfd-48f4-a3e7-892be19c5661
+# ╠═d081bb98-0b70-446f-b544-870ac305fdf8
+# ╠═82d0ca4c-96b3-4792-8cc9-d9e3947fa4b4
+# ╠═0c72e2ea-b6cb-46b1-bbcc-48e0bfa0a618
+# ╠═fe178836-f0d8-409b-baa0-61b34c257cbc
 # ╟─b632e57f-6ee8-424c-a6c0-d75d0a285769
-# ╠═58584903-f737-4654-8e48-0d3b28e19fb9
-# ╠═e216758d-c84e-4cbb-9358-73e31fa53bdb
-# ╠═d03046ef-5cdc-4bae-a9a1-ab3f282d305b
-# ╠═50e3e3e2-aa10-432b-a8db-e9c3fe8298d1
-# ╠═76f3acc5-3209-422c-9d67-8db8b24dd0c3
-# ╠═4e6c750d-2bf9-4f2b-ac28-8b2aeb1cb0d6
-# ╠═a649fb33-53e2-4e93-8267-be2a595a640f
-# ╠═760dca56-97a5-4074-aa1f-b7bdd7a38e2c
-# ╠═ab5213fb-da1f-453e-9db1-1972b829da9c
-# ╠═19feb1b5-f810-4d84-92c3-4425ca157150
-# ╠═87b379c1-0740-4d63-b9d5-13db607d4e61
-# ╠═c2e69ed7-4f54-447b-b592-b65f7a2c567e
-# ╠═5c5673fc-14b6-4f25-85d2-bb4e7aaa434b
-# ╠═d67a7898-4574-422c-8af6-4e148951001b
-# ╠═dc93d51e-cb6c-44e5-9f40-4cdb689146f1
-# ╠═601f71ad-0d74-4708-9200-0834ce04aa8f
-# ╠═bcd4013f-e748-4154-8491-db66e8a8e0b9
-# ╠═98b38ccb-6a09-4aec-abd2-284c1793fc42
-# ╠═27839cba-d644-4180-bfbb-ba60c72038b7
+# ╠═69f7a580-bf68-41c6-ae96-6ecc587e058f
+# ╟─526dd590-2858-4eaa-a9a3-e77375e9ff7f
+# ╠═3187aaf2-0c17-40e0-909c-da5d5bf13a36
+# ╠═f563ce55-fcc6-4b18-9050-1617121d1c52
+# ╟─f110144e-8685-4ac7-bde4-6f31f293981d
+# ╠═e8391325-b7bc-4f4f-b4d6-5637adb75775
+# ╠═3cd09b95-75d7-463d-a3d2-9af10f46eb78
+# ╠═cd47e23d-636a-453e-82eb-bf5bfa12c56d
+# ╟─19feb1b5-f810-4d84-92c3-4425ca157150
+# ╠═13090468-209e-43a0-97c0-6ffb9861f88f
+# ╠═28f5b1f7-a7f3-4278-b02d-fbb0603f52e7
+# ╠═12899136-feef-43bd-867d-42f18a0486ad
+# ╟─d6881fe7-9f91-4ea1-b7b3-00a4121e8445
+# ╠═43080b6d-d0a6-49d3-b8fc-c02daae5bab6
+# ╠═e84d1e3b-4617-49bd-938a-593c77bd24a1
+# ╟─1f5cf7ec-110e-40be-b5ef-a5b6ae33383c
+# ╠═50720c92-2082-45b5-af16-4c6b04b2ed67
+# ╠═f9097463-464a-47ee-aaa3-5501513c044b
+# ╠═61062c66-e4e0-4177-a2cd-21b84ac6c572
+# ╠═c151cb13-b17f-46c9-be32-328c2f794da0
+# ╠═a928a761-91eb-4416-87a6-ca4c52d9636e
+# ╟─87b379c1-0740-4d63-b9d5-13db607d4e61
+# ╠═f3ec83c3-7034-4062-a41d-30c46ec5f309
+# ╟─1d0bf723-3762-43f3-bda7-369ff1b8a326
